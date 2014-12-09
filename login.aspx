2dbbeef8e52440ad1e3cb1e47470328236860291
5n0wD <kevmensink@hotmail.com>
2014-12-09 13:28:00 +0100

    LoginPage + error message + redirect to home

--- login.aspx
+++ login.aspx
@@ -1,15 +1,38 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/aspGebakOpHetWerk/default.Master" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="aspGebakOpHetWerk.aspGebakOpHetWerk.WebForm1" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="CPHHead" runat="server">
-
 </asp:Content>
 
 
 <asp:Content ID="Content2" ContentPlaceHolderID="CPHMenuItems" runat="server">
-
 </asp:Content>
 
 
 <asp:Content ID="Content3" ContentPlaceHolderID="CPHContent" runat="server">
-
+    <table>
+        <tr>
+            <td>
+                <asp:Label ID="lblUsername" Text="Username" runat="server"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="txtUsername" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblPassword" Text="Password" runat="server"></asp:Label>
+            </td>
+            <td>
+                <asp:TextBox ID="txtPassword" TextMode="Password" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Button ID="btnLogin" Text="Login" runat="server" OnClick="btnLogin_Click" />
+            </td>
+            <td>
+                <asp:Label ID="lblError" Text="" runat="server"></asp:Label>
+            </td>
+        </tr>
+    </table>
 </asp:Content>
--- login.aspx.cs
+++ login.aspx.cs
@@ -4,14 +4,69 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+
 
 namespace aspGebakOpHetWerk.aspGebakOpHetWerk
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void btnLogin_Click(object sender, EventArgs e)
         {
+            GebakOphetWerkDBEntities objGebaksModel = new GebakOphetWerkDBEntities();
 
+            string usrString = txtUsername.Text.ToLower();
+            string passwdString = CalculateHashedPassword(txtPassword.Text);
+
+
+            var user = from u in objGebaksModel.gebruikers
+                       where u.password == passwdString && u.userName == usrString
+                       select u;
+
+            if (user.Any())
+            {
+                gebruiker objGebruiker = (gebruiker)user.First();
+
+                //Session maken
+                if (Session.IsNewSession)//als er nog geen session bestaat dit doen
+                {
+                    Session["uID"] = objGebruiker.userID;
+                    Session["role"] = objGebruiker.role;
+
+                    //redirect naar de homepage
+                    Response.Redirect("home.aspx");
+                }
+                else
+                    Response.Redirect("home.aspx");
+            }
+            else
+            {
+                if (txtUsername.Text == "" || txtPassword.Text == "")
+                {
+                    string errortekst = string.Format("Geen gegevens ingevoerd, Voer een gebruikersnaam en een wachtwoord in.");
+                    lblError.Text = errortekst;
+                    txtUsername.Focus();
+                }
+                else
+                {
+                    string errortekst = string.Format("Gebruikersnaam en/of wachtwoord is/zijn incorrect, voer de correcte gebruikersnaam in en/of het juiste wachtwoord");
+                    lblError.Text = errortekst;
+                    txtUsername.Text = "";
+                    txtPassword.Text = "";
+                    txtUsername.Focus();
+                }
+
+            }
+
+        }
+
+        private static string CalculateHashedPassword(string clearpwd)
+        {
+            using (var sha = SHA256.Create())
+            {
+                var computedHash = sha.ComputeHash(System.Text.Encoding.Unicode.GetBytes(clearpwd));
+                return Convert.ToBase64String(computedHash);
+            }
         }
     }
 }
