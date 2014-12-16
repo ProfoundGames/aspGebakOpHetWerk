61349603863cf93d3c6d28eeadd9fd07ae9f4b71
ianno1993 <i.lol@hotmail.com>
2014-12-16 18:46:47 +0100

    I :heart: Github.

--- bestel.aspx.cs
+++ bestel.aspx.cs
@@ -14,7 +14,6 @@ namespace aspGebakOpHetWerk.aspGebakOpHetWerk
 
         protected void Page_Load(object sender, EventArgs e)
         {
-<<<<<<< HEAD
             //De GridView vullen
             int? tempOrderID = Convert.ToInt32(Session["currentOrderID"]);
 
@@ -27,12 +26,9 @@ namespace aspGebakOpHetWerk.aspGebakOpHetWerk
                 dgvBestellingLijst.DataSource = entity.GetOrderList(Convert.ToInt32(Session["currentOrderID"]));
                 dgvBestellingLijst.DataBind();
             }
-=======
->>>>>>> origin/master
 
             if (Session["currentOrderID"] == null)
             {
-
                 int usrString = Convert.ToInt32(Session["uID"]);
                 var user = from u in entity.gebruikers
                            where u.userID == usrString
@@ -45,7 +41,6 @@ namespace aspGebakOpHetWerk.aspGebakOpHetWerk
                     orderDate = DateTime.Today,
                     gebruiker = objGebruiker
                 });
-
                 entity.SaveChanges();
 
                 if (entity.GetOrderIdList((int)Session["uID"]) != null)
@@ -60,11 +55,11 @@ namespace aspGebakOpHetWerk.aspGebakOpHetWerk
                     Response.Redirect("notificatie.aspx");
                 }
 
-                
+
 
             }
 
-            if(!IsPostBack)
+            if (!IsPostBack)
             {
                 ddlTaartSoort.DataSource = entity.GetTaartenList();
                 ddlTaartSoort.DataTextField = "name";
@@ -98,19 +93,7 @@ namespace aspGebakOpHetWerk.aspGebakOpHetWerk
                                   select T;
                     taart objTaart = idTaart.First();
 
-<<<<<<< HEAD
                     orderItem objOrderItem = new orderItem();
-=======
-                    entity.orderItems.Add(new orderItem
-                    {
-                        cakeID = Convert.ToInt32(ddlTaartSoort.SelectedValue),
-                        amount = Convert.ToInt32(txtAmount.Text),
-                        orderID = Convert.ToInt32(Session["currentOrderID"]),
-                        TotalAmountOfMoney = CalculateTotalAmount(),
-
-                        order = objOrder,
-                        taart = objTaart
->>>>>>> origin/master
 
                     objOrderItem.cakeID = Convert.ToInt32(ddlTaartSoort.SelectedValue);
                     objOrderItem.orderID = objOrder.orderID;
